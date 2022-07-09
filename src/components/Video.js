6f1b05ba37dc64a3a22fd19dbf734b7e8936d9c4
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-07-09 23:15:01 +0300

    added

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../modalVideo.scss';
+import ModalVideo from 'react-modal-video';
 
 const Video = () => {
+  const [isOpen, setOpen] = useState(false);
   return (
-    <div className='bg-[#534686]/30 bg-no-repeat py-12'>
+    <div className='bg-[#534686]/30 bg-no-repeat py-6'>
       <div className='container mx-auto'>
-        <div className='flex justify-between'>
-          <h3 className='text-2xl font-semibold'>
+        <div className='flex flex-col justify-center items-center text-center lg:flex-row lg:justify-between'>
+          <h3 className='text-2xl font-semibold mb-8 lg:mb-0'>
             Awesome experiences with virtual reality world
           </h3>
-          <div>video</div>
+          <ModalVideo
+            channel='youtube'
+            autoplay
+            isOpen={isOpen}
+            videoId='NOk_M1Ib5F0'
+            onClose={() => setOpen(false)}
+          />
+          <div className='bg-videoBg bg-no-repeat bg-cover w-[270px] h-[180px]'>
+            <div onClick={() => setOpen(!isOpen)} className='cursor-pointer'>
+              play
+            </div>
+          </div>
         </div>
       </div>
     </div>
