45c50966c039fb08fffbd92e1f15b9f18640e50d
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-07-10 12:24:50 +0300

    added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+// import logo
+import Logo from '../assets/img/logo.svg';
 const Header = () => {
   return (
-    <header data-aos='fade-down' data-aos-duration='2000' data-aos-delay='900'>
-      <div className='container mx-auto'>header</div>
+    <header
+      className='py-6'
+      data-aos='fade-down'
+      data-aos-duration='2000'
+      data-aos-delay='900'
+    >
+      <div className='container mx-auto'>
+        {/* logo */}
+        <a href='#'>
+          <img className='h-[30px]' src={Logo} alt='' />
+        </a>
+      </div>
     </header>
   );
 };
--- src/components/Users.js
+++ src/components/Users.js
@@ -30,7 +30,7 @@ const Users = () => {
         </div>
       </div>
       <div className='flex items-center space-x-2 font-secondary font-medium'>
-        <BsFillCircleFill className='text-xs text-green-400 animate-pulse' />
+        <BsFillCircleFill className='text-xs text-green-500 animate-pulse' />
         <div>400k people online</div>
       </div>
     </div>
