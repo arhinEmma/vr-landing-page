7b947603b6ac7a26cd6862bbe6970e12b594ac6c
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-07-11 22:55:07 +0300

    added

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -3,14 +3,16 @@ import { TestimonialSlider } from './TestimonialSlider';
 
 const Testimonial = () => {
   return (
-    <section className='bg-purple-400/10 max-h-[400px] py-12'>
+    <section>
       <div className='container mx-auto'>
-        <h2 className='text-3xl font-bold mb-6'>What our clients say</h2>
-        <p className='max-w-2xl mb-24'>
-          See what our customer say about us. It really matter for us. How good
-          or bad we will make ir for evaluation to make EhyalLive better.
-        </p>
-        <TestimonialSlider />
+        <div className='bg-purple-400/10 max-h-[400px] p-12'>
+          <h2 className='text-3xl font-bold mb-6'>What our clients say</h2>
+          <p className='max-w-2xl mb-24'>
+            See what our customer say about us. It really matter for us. How
+            good or bad we will make ir for evaluation to make EhyalLive better.
+          </p>
+          <TestimonialSlider />
+        </div>
       </div>
     </section>
   );
