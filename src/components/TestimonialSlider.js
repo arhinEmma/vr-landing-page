ddec66dba2f5fad1d7369350fdd3caf79906a9e1
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-07-12 13:51:42 +0300

    added

--- src/components/TestimonialSlider.js
+++ src/components/TestimonialSlider.js
@@ -1,15 +1,14 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-
 // import './styles.css';
-
 // import required modules
 import { Pagination } from 'swiper';
+// import data
+import { slider } from '../data';
 
 export const TestimonialSlider = () => {
   return (
@@ -19,26 +18,26 @@ export const TestimonialSlider = () => {
       modules={[Pagination]}
       className='mySwiper'
     >
-      {/* linear-gradient(0deg, #130613 0%, #2A1428 51.15%, #331A35 102.63%); */}
-
-      <SwiperSlide className='min-h-[240px] bg-gradient-to-t from-[#130613] via-[#2A1428] to-[#331A35]'>
-        Slide 1
-      </SwiperSlide>
-      <SwiperSlide className='min-h-[240px] bg-gradient-to-t from-[#130613] via-[#2A1428] to-[#331A35]'>
-        Slide 2
-      </SwiperSlide>
-      <SwiperSlide className='min-h-[240px] bg-gradient-to-t from-[#130613] via-[#2A1428] to-[#331A35]'>
-        Slide 3
-      </SwiperSlide>
-      <SwiperSlide className='min-h-[240px] bg-gradient-to-t from-[#130613] via-[#2A1428] to-[#331A35]'>
-        Slide 4
-      </SwiperSlide>
-      <SwiperSlide className='min-h-[240px] bg-gradient-to-t from-[#130613] via-[#2A1428] to-[#331A35]'>
-        Slide 5
-      </SwiperSlide>
-      <SwiperSlide className='min-h-[240px] bg-gradient-to-t from-[#130613] via-[#2A1428] to-[#331A35]'>
-        Slide 6
-      </SwiperSlide>
+      {slider.map((slide, index) => {
+        const { message, image, name, email } = slide;
+        return (
+          <SwiperSlide
+            className='rounded-lg p-6 min-h-[240px] bg-gradient-to-t from-[#130613] via-[#2A1428] to-[#331A35]'
+            key={index}
+          >
+            <p className='mb-8 min-h-[100px] text-[15px]'>{message}</p>
+            <div className='flex items-center gap-x-4'>
+              <div className='w-14'>
+                <img src={image} alt='' />
+              </div>
+              <div>
+                <div className='font-medium text-base'>{name}</div>
+                <div className='font-medium text-rose-600'>{email}</div>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
 };
